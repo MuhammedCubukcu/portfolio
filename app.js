f2f02fb6ddcf6fe19e40ac9e08bb3645a76be655
MuhammedCubukcu <muhammedcubukcu0@gmail.com>
2024-08-07 00:47:26 +0300

    portfolio website

---
+++ app.js
@@ -0,0 +1,17 @@
+
+function githubPage(){
+    window.location.href="https://github.com/MuhammedCubukcu"
+}
+
+
+function sendEmail(){
+    var email = "muhammedcubukcu0@gmail.com";
+    var subject = "Subject Text";
+    var body = "Body text here.";
+    window.location.href = `https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(email)}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+}
+
+
+function linkedinPage(){
+      window.location.href="https://www.linkedin.com/in/muhammed-cubukcu/"
+}
